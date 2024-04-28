fb7be78564477a379eca89c9342d6c79b0bcfa74
2duckchun <kkts9308@gmail.com>
2024-04-28 22:31:51 +0900

    🚧 page에 form 관련 코드 제거 및 state를 이용해 modal 열 수 있도록 수정

--- src/app/(routes)/input/page.tsx
+++ src/app/(routes)/input/page.tsx
@@ -1,30 +1,40 @@
 'use client'
 
-import { FunctionComponent } from 'react'
+import { SecondTextModal } from '@/components/modal/SecondTextModal'
+import { TestModal } from '@/components/modal/TestModal'
+import { CForm } from '@/components/ui/CustomForm'
+import { FunctionComponent, useState } from 'react'
 
 interface PageProps {}
 
 const Page: FunctionComponent<PageProps> = ({}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClose = () => {
+    setIsOpen(() => false)
+  }
+
   const onSubmit = (data: FormData) => {
     console.log(Object.fromEntries(data.entries()))
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const formElement = e.target as HTMLFormElement
-    const dataObject = new FormData(formElement)
-    onSubmit(dataObject)
-  }
-
   return (
     <main>
-      <form className="h-[50px]" onSubmit={handleSubmit}>
+      <SecondTextModal isOpen={isOpen} onClose={onClose} />
+      <CForm onSubmit={onSubmit} className="flex h-[100px] gap-5 bg-slate-700">
         <input name="a"></input>
         <input name="b"></input>
         <input name="c"></input>
         <input name="d"></input>
         <button type="submit">눌러</button>
-      </form>
+      </CForm>
+      <button
+        onClick={() => {
+          setIsOpen((state) => !state)
+        }}
+      >
+        눌르라
+      </button>
     </main>
   )
 }
